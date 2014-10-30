b22298869864ebf876513027ea6f21e6ae2e17c6
block_given? <block_given@outlook.com>
2014-10-31 01:24:54 +0900

    Add ruboty test adapter and ruboty testable module

---
+++ spec/support/ruboty-testable.rb
@@ -0,0 +1,20 @@
+module Ruboty
+  module Adapters
+    class Mock < Base
+      def run
+      end
+    end
+  end
+
+  module Testable
+    def say(message)
+      said << message
+    end
+
+    def said
+      @said ||= []
+    end
+  end
+
+  Robot.prepend(Testable)
+end
